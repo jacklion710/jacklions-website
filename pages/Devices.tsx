98ff014f35f5a4f60374021c1f55507a456d9f65
jacklion710 <jack.lion710@gmail.com>
2023-08-07 18:04:46 -0400

    "Added gumroad embeddings for devices page"

--- pages/Devices.tsx
+++ pages/Devices.tsx
@@ -7,88 +7,103 @@ import {
   Text,
   Image,
   Button,
-  Link
+  Link,
+  useColorModeValue
 } from '@chakra-ui/react';
 import Navbar from '../components/Navbar';
 
 export default function DevicesPage() {
+
+  // Define a sample pack array to map through
+  const devices = [
+    {
+      name: "M4L MAP Button",
+      description: "A simple, optimized UI snippet for adding a MAP button to your max for live creations. Maps any process in a max device to any parameter in Ableton.",
+      gumroadLink: "https://jacklion.gumroad.com/l/basicm4lmapper"
+    },
+    {
+      name: "Tune Delay",
+      description: "A fun tool for mapping MIDI notes to delay periods that coincide with the input notes. Creative delay for 'tuned' delay times.",
+      gumroadLink: "https://jacklion.gumroad.com/l/tunedelay"
+    },
+    {
+      name: "Transfer Curve Macro",
+      description: "A special macro designed to allow users to assign a custom multi-point transfer curve to a macro. Macro output can follow a unique non-linear path",
+      gumroadLink: "https://jacklion.gumroad.com/l/jlcurvefuncmacro"
+    },
+    {
+      name: "Hackro Rack",
+      description: "A modified transfer curve macro device that accomodates up to 16 parameter mappings in Ableton with non-linear transfer curves",
+      gumroadLink: "https://jacklion.gumroad.com/l/jlhackrorack"
+    },
+    {
+      name: "Remote FM",
+      description: "An experimental device used to route signals remotely across any channels in Ableton for frequency modulation.",
+      gumroadLink: "https://app.gumroad.com/products/nZuKi"
+    },
+    {
+      name: "Self FM",
+      description: "A weird sounding device used to route signals into themselves in Ableton for frequency modulation feedback.",
+      gumroadLink: "https://app.gumroad.com/products/nRDNZ"
+    },
+    {
+      name: "Jitter Synchronized Keyframe Editor",
+      description: "A tool used for synching animations in jitter to a timeline of a track. Can also be used to sequence msp and control signals",
+      gumroadLink: "https://app.gumroad.com/products/spwig"
+    },
+    {
+      name: "Repitch BPM Calculator",
+      description: "A useful calculator for sample based productions that require precise control over a resulting pitch change from varitempo style repotching of clip tempo",
+      gumroadLink: "  https://app.gumroad.com/products/PHHsI      "
+    },
+    {
+      name: "Sonogram",
+      description: "A simple and free visualizer for frequency content over time",
+      gumroadLink: "https://app.gumroad.com/products/AoHOm      "
+    },
+    {
+      name: "Lissajous Phase Correlation Scope",
+      description: "A scope used for visualizing lissajous XY phase correlation patterns from stereo signals",
+      gumroadLink: "https://app.gumroad.com/products/wyBWZ"
+    },
+    {
+      name: "Oscilloscope",
+      description: "A basic and free oscilloscope for visualizing time-domain audio",
+      gumroadLink: "https://app.gumroad.com/products/sDFcL"
+    },
+    {
+      name: "Stereo Pitch Shifter",
+      description: "A creative stereo pitch shifting device which gives users independent control of pitch per stereo channel",
+      gumroadLink: "https://app.gumroad.com/products/vPZjV"
+    }
+  ];
+  const iframeBg = useColorModeValue("white", "gray.900"); // Chakra Color mode support
+
   return (
     <ChakraProvider>
       <Navbar />
       <Flex justify="center" my={8} ml={0} flexGrow={1}>
-        <VStack spacing={10} width="100%" maxW="1000px">
+        <VStack spacing={250} width="50%" maxW="1000px">
           <Heading size="xl" borderBottom="1px solid">Creative Devices</Heading>
 
-          {/* Device Item */}
-          <Box p={4} shadow="lg" borderWidth="1px" borderRadius="md" width="100%">
-            <Flex justify="space-between" align="center">
-              <Box>
-                <Heading size="lg" mb={4}>Tune Delay</Heading>
-                <Text>Device Description</Text>
-              </Box>
-              <Box>
-                <Image src="/path/to/samplepack/image.jpg" alt="Sample Pack Image" boxSize="100px" />
-              </Box>
-            </Flex>
-            <Button colorScheme="blue" size="md" mt={4}>
-              <Link href="/path/to/purchase">
-                Purchase
-              </Link>
-            </Button>
-          </Box>
-
-          <Box p={4} shadow="lg" borderWidth="1px" borderRadius="md" width="100%">
-            <Flex justify="space-between" align="center">
-              <Box>
-                <Heading size="lg" mb={4}>M4L Basic MAP Button</Heading>
-                <Text>Device Description</Text>
-              </Box>
-              <Box>
-                <Image src="/path/to/samplepack/image.jpg" alt="Sample Pack Image" boxSize="100px" />
-              </Box>
-            </Flex>
-            <Button colorScheme="blue" size="md" mt={4}>
-              <Link href="/path/to/purchase">
-                Purchase
-              </Link>
-            </Button>
-          </Box>
-
-          <Box p={4} shadow="lg" borderWidth="1px" borderRadius="md" width="100%">
-            <Flex justify="space-between" align="center">
-              <Box>
-                <Heading size="lg" mb={4}>FM Anything</Heading>
-                <Text>Device Description</Text>
-              </Box>
-              <Box>
-                <Image src="/path/to/samplepack/image.jpg" alt="Sample Pack Image" boxSize="100px" />
-              </Box>
-            </Flex>
-            <Button colorScheme="blue" size="md" mt={4}>
-              <Link href="/path/to/purchase">
-                Purchase
-              </Link>
-            </Button>
-          </Box>
-          
-          <Box p={4} shadow="lg" borderWidth="1px" borderRadius="md" width="100%">
-            <Flex justify="space-between" align="center">
-              <Box>
-                <Heading size="lg" mb={4}>Spectrogram</Heading>
-                <Text>Device Description</Text>
-              </Box>
-              <Box>
-                <Image src="/path/to/samplepack/image.jpg" alt="Sample Pack Image" boxSize="100px" />
-              </Box>
-            </Flex>
-            <Button colorScheme="blue" size="md" mt={4}>
-              <Link href="/path/to/purchase">
-                Purchase
-              </Link>
-            </Button>
-          </Box>
-
-          {/* You can replicate the above Box for other sample packs or map through an array of sample packs to display them. */}
+          {devices.map(dev => (
+            <Box key={dev.name} p={4} shadow="lg" borderWidth="1px" borderRadius="md" width="100%" height="500px">
+              <Flex justify="space-between" align="center">
+                <Box>
+                  <Heading size="lg" mb={4}>{dev.name}</Heading>
+                  <Text>{dev.description}</Text>
+                </Box>
+              </Flex>
+              <iframe 
+                src={dev.gumroadLink} 
+                width="100%" 
+                height="580"   // Adjusted from 240 to 360
+                frameBorder="0" 
+                scrolling="no" 
+                style={{ background: iframeBg, marginTop: "16px" }} 
+                title={`Purchase ${dev.name}`}></iframe>
+            </Box>
+          ))}
 
         </VStack>
       </Flex>
