55faf71be3bb2757e5c97aa59ab9c178252c22dc
jacklion710 <jack.lion710@gmail.com>
2023-08-06 23:35:09 -0400

    "Initial shop configuration"

--- pages/Samples.tsx
+++ pages/Samples.tsx
@@ -1,17 +1,46 @@
-import React from 'react';
+import {
+  Box,
+  Heading,
+  VStack,
+  ChakraProvider,
+  Flex,
+  Text,
+  Image,
+  Button,
+  Link
+} from '@chakra-ui/react';
+import Navbar from '../components/Navbar';
 
-const Music = () => {
+export default function SamplePacksPage() {
   return (
-    <div style={{ padding: '20px', textAlign: 'center' }}>
-      <h2>Your Music Title Here</h2>
-      <p>Description or any other details about the music.</p>
-      
-      <audio controls>
-        <source src="path_to_your_audio_file.mp3" type="audio/mpeg" />
-        Your browser does not support the audio element.
-      </audio>
-    </div>
+    <ChakraProvider>
+      <Navbar />
+      <Flex justify="center" my={8} ml={0} flexGrow={1}>
+        <VStack spacing={10} width="100%" maxW="1000px">
+          <Heading size="xl" borderBottom="1px solid">Sample Packs</Heading>
+
+          {/* Sample Pack Item */}
+          <Box p={4} shadow="lg" borderWidth="1px" borderRadius="md" width="100%">
+            <Flex justify="space-between" align="center">
+              <Box>
+                <Heading size="lg" mb={4}>Sample Pack Name</Heading>
+                <Text>Sample Pack Description</Text>
+              </Box>
+              <Box>
+                <Image src="/path/to/samplepack/image.jpg" alt="Sample Pack Image" boxSize="100px" />
+              </Box>
+            </Flex>
+            <Button colorScheme="blue" size="md" mt={4}>
+              <Link href="/path/to/purchase">
+                Purchase
+              </Link>
+            </Button>
+          </Box>
+
+          {/* You can replicate the above Box for other sample packs or map through an array of sample packs to display them. */}
+
+        </VStack>
+      </Flex>
+    </ChakraProvider>
   );
 }
-
-export default Music;
