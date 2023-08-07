629581ee8e398706c15f2045f99a520392150f9a
jacklion710 <jack.lion710@gmail.com>
2023-08-07 19:02:24 -0400

    "removed Link sections"

--- pages/Samples.tsx
+++ pages/Samples.tsx
@@ -6,7 +6,6 @@ import {
   Flex,
   Text,
   Image,
-  Button,
   Link
 } from '@chakra-ui/react';
 import Navbar from '../components/Navbar';
@@ -20,21 +19,68 @@ export default function SamplePacksPage() {
           <Heading size="xl" borderBottom="1px solid">Sample Packs</Heading>
 
           {/* Sample Pack Item */}
-          <Box p={4} shadow="lg" borderWidth="1px" borderRadius="md" width="100%">
+          <Box p={4} shadow="lg" borderWidth="1px" borderRadius="md" width="70%">
             <Flex justify="space-between" align="center">
               <Box>
-                <Heading size="lg" mb={4}>Breakbeats</Heading>
-                <Text>Sample Pack Description</Text>
-              </Box>
-              <Box>
-                <Image src="/path/to/samplepack/image.jpg" alt="Sample Pack Image" boxSize="100px" />
+                <Heading size="lg" mb={4}>Organic Percs</Heading>
+                <Text>Acoustic percussive sounds of anything I could get my hands on in a local music school</Text>
               </Box>
             </Flex>
-            <Button colorScheme="blue" size="md" mt={4}>
-              <Link href="/path/to/purchase">
-                Purchase
-              </Link>
-            </Button>
+            <Box mt={4}>
+              <iframe 
+                style={{border: "0", width: "100%", height: "800px"}} 
+                src="https://bandcamp.com/EmbeddedPlayer/album=2258007380/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" 
+                seamless>
+              </iframe>
+            </Box>
+          </Box>
+
+          <Box p={4} shadow="lg" borderWidth="1px" borderRadius="md" width="70%">
+            <Flex justify="space-between" align="center">
+              <Box>
+                <Heading size="lg" mb={4}>Anulo</Heading>
+                <Text>Analog eurorack experimentation</Text>
+              </Box>
+            </Flex>
+            <Box mt={4}>
+              <iframe 
+                style={{border: "0", width: "100%", height: "800px"}} 
+                src="https://bandcamp.com/EmbeddedPlayer/album=3513673397/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" 
+                seamless>
+              </iframe>
+            </Box>
+          </Box>
+
+          <Box p={4} shadow="lg" borderWidth="1px" borderRadius="md" width="70%">
+            <Flex justify="space-between" align="center">
+              <Box>
+                <Heading size="lg" mb={4}>What Goes Bump</Heading>
+                <Text>Miscellaneous electronic sounds. One-shots, fx percs and ear candy</Text>
+              </Box>
+            </Flex>
+            <Box mt={4}>
+              <iframe 
+                style={{border: "0", width: "100%", height: "800px"}} 
+                src="https://bandcamp.com/EmbeddedPlayer/album=4003857503/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" 
+                seamless>
+              </iframe>
+            </Box>
+          </Box>
+
+          <Box p={4} shadow="lg" borderWidth="1px" borderRadius="md" width="70%">
+            <Flex justify="space-between" align="center">
+              <Box>
+                <Heading size="lg" mb={4}>Free Foley Pack</Heading>
+                <Text>Recorded foley and FX</Text>
+              </Box>
+            </Flex>
+            <Box mt={4}>
+              <iframe 
+                style={{border: "0", width: "100%", height: "800px"}} 
+                src="https://bandcamp.com/EmbeddedPlayer/album=797578818/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" 
+                seamless>
+              </iframe>
+            </Box>
           </Box>
 
           {/* You can replicate the above Box for other sample packs or map through an array of sample packs to display them. */}
@@ -44,3 +90,4 @@ export default function SamplePacksPage() {
     </ChakraProvider>
   );
 }
+
