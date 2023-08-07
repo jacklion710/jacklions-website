0592c9f9e19854cbde2de2d5b1d6984e50ebe36e
jacklion710 <jack.lion710@gmail.com>
2023-08-06 23:26:08 -0400

    "Added details and links"

--- pages/Engineering.tsx
+++ pages/Engineering.tsx
@@ -1,17 +1,103 @@
-import React from 'react';
-
-const Music = () => {
+import {
+  Box,
+  Heading,
+  Link,
+  VStack,
+  ChakraProvider,
+  Flex,
+  Text,
+  Button,
+  List,
+  ListItem
+} from '@chakra-ui/react';
+import Navbar from '../components/Navbar';
+export default function EngineeringPage() {
   return (
-    <div style={{ padding: '20px', textAlign: 'center' }}>
-      <h2>Your Music Title Here</h2>
-      <p>Description or any other details about the music.</p>
-      
-      <audio controls>
-        <source src="path_to_your_audio_file.mp3" type="audio/mpeg" />
-        Your browser does not support the audio element.
-      </audio>
-    </div>
+      <ChakraProvider>
+          <Navbar />
+          <Flex justify="center" my={8} ml={0} flexGrow={1}>
+              <VStack spacing={10} width="100%" maxW="600px">
+                  <Heading size="xl" borderBottom="1px solid">Recording Studio Catalog</Heading>
+
+                  <Box p={4} shadow="lg" borderWidth="1px" borderRadius="md" width="100%">
+                      <Heading size="lg" mb={4}>Recording</Heading>
+                      <Text>Professional vocal recordings or mono, stereo, mid/side, and up to 16 channel multitrack instrumental recording sessions for any source you wish to capture.</Text>
+                      
+                      <Heading size="md" mt={4}>Instruments Available:</Heading>
+                      <List>
+                          <Text>Electric Guitar</Text>
+                          <Text>5 String Electric Bass</Text>
+                          <Text>Acoustic Guitar</Text>
+                      </List>
+
+                      <Heading size="md" mt={4}>Electronic Instruments:</Heading>
+                      <List>
+                          <Text>Subsequent37</Text>
+                          <Text>Subharmonicon</Text>
+                          <Text>DFAM</Text>
+                          <Text>Eurorack</Text>
+                      </List>
+
+                      <Heading size="md" mt={4}>Microphones:</Heading>
+                      <List>
+                          <Text>Telefunken TF39</Text>
+                          <Text>Aston Origin</Text>
+                          <Text>SM57</Text>
+                          <Text>SM7B</Text>
+                          <Text>CR-14</Text>
+                          <Text>SP-1</Text>
+                      </List>
+                      
+                      <Text mt={4}>Minimum of 1 hr in order to book a recording session</Text>
+                      <Text>$50/hr</Text>
+                      <Text>$140 for 3 hours</Text>
+                  </Box>
+
+                  <Box p={4} shadow="lg" borderWidth="1px" borderRadius="md" width="100%">
+                      <Heading size="lg" mb={4}>Engineering</Heading>
+                      <Text>Choose from a variety of mixdown and master options to get the perfect sound for your project.</Text>
+                      <Text>Mixdown</Text>
+                      <Text>Digital Mastering</Text>
+                      <Text>Analog Mastering</Text>
+                  </Box>
+                  <Box p={4} shadow="lg" borderWidth="1px" borderRadius="md" width="100%">
+                      <Heading size="lg" mb={4}>Sound Design</Heading>
+                      <Text>Customized sound design for your tracks from synth parts & patches to soundscapes and cinematic fx.</Text>
+                      <Text>Analog Synths</Text>
+                      <Text>Drum Machines</Text>
+                      <Text>VST Patches</Text>
+                      <Text>FX</Text>
+                      <Text>Synth Patches</Text>
+                      <Text>Foley</Text>
+                      <Text>Electric or Acoustic Guitar</Text>
+                      <Text>Electric Bass</Text>
+                      
+                  </Box>
+                  <Box p={4} shadow="lg" borderWidth="1px" borderRadius="md" width="100%">
+                      <Heading size="lg" mb={4}>Consultations</Heading>
+                      <Text>Customized sound design for your tracks from synth parts & patches to soundscapes and cinematic fx.</Text>
+                      <Text>Track Feedback</Text>
+                      <Text>Art Installation Planning</Text>
+                      <Text>DSP Theory & Design</Text>
+                  </Box>
+
+                  <Button colorScheme="blue" size="md" mt={4}>
+                      <Link href="https://calendar.google.com/calendar/u/0/appointments/schedules/AcZssZ0w9Y-bSp4iEfmtZLVteHAJSyjUOXOf_pLSQx0JAN_XalobFO2LVchz5a_qWVwUqusj5KcKVPth" isExternal>
+                          Book Now
+                      </Link>
+                  </Button>
+
+                  <Button colorScheme="blue" size="md" mt={4}>
+                      <Link href="/Direct">
+                          Contact & Pricing
+                      </Link>
+                  </Button>
+
+              </VStack>
+          </Flex>
+      </ChakraProvider>
   );
 }
 
-export default Music;
+
+// Add dropdowns within each box with sound examples
