298b6101cd36a20bf24f4f1d58aa4f1ce6876c2e
jacklion710 <jack.lion710@gmail.com>
2023-08-06 22:42:49 -0400

    "Added underline to header"

--- pages/Direct.tsx
+++ pages/Direct.tsx
@@ -28,13 +28,32 @@ export default function SocialPage() {
       <Flex
         direction="column"
         align="center"
-        justify="center"
+        justify="flex-start"
         py={8}
         px={4}
         height="calc(100vh - 100px)"
       >
-        <Box w="100%" maxW="600px" textAlign="center">
-          <Heading size="xl" mb={4}>
+        <Box 
+          w="100%" 
+          maxW="600px" 
+          textAlign="center" 
+          position="relative"
+        >
+          <Heading 
+            size="xl" 
+            mb={4} 
+            position="relative"
+            _after={{
+              content: '""',
+              display: 'block',
+              position: 'absolute',
+              bottom: '-10px',  // Adjust this value to move the border closer/farther from the text
+              left: '50%',
+              transform: 'translateX(-50%)',
+              width: '290px',  // Adjust this width as per your requirements
+              borderBottom: `1px solid `,
+            }}
+          >
             Connect with Me
           </Heading>
           <Text mb={6}>
@@ -56,7 +75,7 @@ export default function SocialPage() {
             <Text fontSize="lg" fontWeight="semibold">
               For bookings, please use the link below:
             </Text>
-            <Link href="YOUR_BOOKING_LINK_HERE" isExternal>
+            <Link href="BOOKING_LINK" isExternal>
               <Button colorScheme="blue" size="lg">
                 Book Now
               </Button>
