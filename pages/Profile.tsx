7fec6454fa687dddb88a34013b5ae1c427b698ac
jacklion710 <jack.lion710@gmail.com>
2023-08-10 17:25:50 -0400

    "Display basic user data"

--- pages/Profile.tsx
+++ pages/Profile.tsx
@@ -1,18 +1,59 @@
-import { Box, Flex, Heading, Text } from '@chakra-ui/react';
-import Navbar from '../components/Navbar';  // Adjust the path based on your folder structure
+import { Box, ChakraProvider, Flex, Heading, Text, HStack } from '@chakra-ui/react';
+import Navbar from '../components/Navbar';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { useState, useEffect } from 'react';
+import { getFirestore, doc, setDoc, getDoc } from 'firebase/firestore';
+import { auth } from '@/utils/firebase';
+
+interface User {
+  email: string | null;
+  displayName: string | null;
+}
 
 const Profile = () => {
-  return (
-    <Box>
-      <Navbar />
+  const [user, setUser] = useState<User | null>(null);
+  const backgroundColor = 'gray.900';  // Dark background for entire page
+  const boxColor = 'gray.700';  // Dark background for the Box component
 
-      <Flex direction="column" align="center" justify="center" minHeight="calc(100vh - 64px)">
-        <Heading mb={4}>Your Profile</Heading>
-        <Text>Username: JohnDoe</Text>
-        <Text>Email: johndoe@example.com</Text>
-        {/* More profile details */}
-      </Flex>
-    </Box>
+  useEffect(() => {
+    // Set an authentication state observer and get user data
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setUser({
+          email: currentUser.email,
+          displayName: currentUser.displayName,
+        });
+      } else {
+        setUser(null);
+      }
+    });
+
+    // Cleanup the listener on component unmount
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <ChakraProvider>
+        <Box bgColor={backgroundColor} minH="100vh">
+            <Navbar />
+
+            <Flex direction="column" align="center" justify="center" minHeight="calc(100vh - 64px)">
+                <Heading mb={4} color="white">Your Profile</Heading>
+
+                <Box w="full" maxW="600px" p={4} bgColor={boxColor} borderWidth={1} borderRadius="lg" boxShadow="lg">
+                    {user ? (
+                        <HStack spacing={4}>
+                            <Text color="white">Username: {user.displayName || 'Not set'}</Text>
+                            <Text color="white">Email: {user.email}</Text>
+                            {/* More profile details */}
+                        </HStack>
+                    ) : (
+                        <Text color="white">Loading or not logged in...</Text>
+                    )}
+                </Box>
+            </Flex>
+        </Box>
+    </ChakraProvider>
   );
 }
 
