896bd20faaa2cacb6b8dc723f89cd87e67681591
jacklion710 <jack.lion710@gmail.com>
2023-08-10 15:31:34 -0400

    "Deleted invalid comment"

--- pages/Social.tsx
+++ pages/Social.tsx
@@ -27,7 +27,7 @@ export default function SocialPage() {
               bottom: '-5px',
               left: '50%',
               transform: 'translateX(-50%)',
-              width: '0px', // Fixed width for demonstration
+              width: '0px', 
               borderBottom: '1px solid',
             }}
           >
@@ -39,25 +39,31 @@ export default function SocialPage() {
                 <Link href="https://open.spotify.com/artist/35foCh1HOk7XwvVzuiFmzc" isExternal>
                     <IconButton aria-label="Spotify" icon={<FaSpotify />} size="lg" colorScheme="green" />
                 </Link>
+                
                 <Link href="https://music.apple.com/us/artist/jack-lion/1470477992" isExternal>
                     <IconButton aria-label="Apple Music" icon={<FaApple />} size="lg" colorScheme="gray" />
                 </Link>
+
                 <Link href="https://soundcloud.com/jack0lion" isExternal>
                     <IconButton aria-label="SoundCloud" icon={<FaSoundcloud />} size="lg" colorScheme="red" />
                 </Link>
+
                 <Link href="https://www.youtube.com/channel/UCbTxhDz-oFPdbKl5-rpi4gQ" isExternal>
                     <IconButton aria-label="YouTube" icon={<FaYoutube />} size="lg" colorScheme="red" />
                 </Link>
+
                 <Link href="https://www.instagram.com/jack.lion/?hl=en" isExternal>
                     <IconButton aria-label="Instagram" icon={<FaInstagram />} size="lg" colorScheme="pink" />
                 </Link>
+
                 <Link href="https://www.patreon.com/jacklion" isExternal>
                     <IconButton aria-label="Patreon" icon={<FaPatreon />} size="lg" colorScheme="purple" />
                 </Link>
+
                 <Link href="https://github.com/jacklion710" isExternal>
                     <IconButton aria-label="GitHub" icon={<FaGithub />} size="lg" colorScheme="gray" />
                 </Link>
-                // ... Add other platforms similarly ...
+
               </HStack>
            </Flex>
     </ChakraProvider>
