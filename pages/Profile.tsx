7bdf618609e46af65537a631e036e384490298f9
jacklion710 <jack.lion710@gmail.com>
2023-08-11 13:54:29 -0400

    "Added delete account functionality to profile"

--- pages/Profile.tsx
+++ pages/Profile.tsx
@@ -1,6 +1,6 @@
-import { Box, ChakraProvider, Flex, Heading, Text, HStack } from '@chakra-ui/react';
+import { Box, ChakraProvider, Flex, Heading, Text, HStack, Button, useToast } from '@chakra-ui/react';
 import Navbar from '../components/Navbar';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getAuth, onAuthStateChanged, deleteUser } from 'firebase/auth';
 import { useState, useEffect } from 'react';
 import { getFirestore, doc, setDoc, getDoc } from 'firebase/firestore';
 import { auth } from '@/utils/firebase';
@@ -10,50 +10,73 @@ interface User {
   displayName: string | null;
   username: string | null;
 }
+
 const Profile = () => {
   const [user, setUser] = useState<User | null>(null);
-  const backgroundColor = 'gray.900';  // Dark background for entire page
-  const boxColor = 'gray.700';  // Dark background for the Box component
+  const backgroundColor = 'gray.900'; 
+  const boxColor = 'gray.700';  
+  const toast = useToast();
 
   useEffect(() => {
-    // Set an authentication state observer and get user data
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       if (currentUser) {
         setUser({
           email: currentUser.email,
           displayName: currentUser.displayName,
-          username: null  // added this line
+          username: null  
         });
       } else {
         setUser(null);
       }
     });
 
-    // Cleanup the listener on component unmount
     return () => unsubscribe();
-}, []);
+  }, []);
+
+  const handleDeleteAccount = async () => {
+    if (user && auth.currentUser && window.confirm('Are you sure you want to delete your account?')) {
+      try {
+        await deleteUser(auth.currentUser);
+        toast({
+          title: "Account deleted.",
+          description: "Your account has been successfully deleted.",
+          status: "success",
+          duration: 5000,
+          isClosable: true,
+        });
+      } catch (error: any) {
+        toast({
+          title: "Error.",
+          description: error.message,
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+      }
+    }
+  };
 
   return (
     <ChakraProvider>
-        <Box bgColor={backgroundColor} minH="100vh">
-            <Navbar />
+      <Box bgColor={backgroundColor} minH="100vh">
+        <Navbar />
 
-            <Flex direction="column" align="center" justify="center" minHeight="calc(100vh - 64px)">
-                <Heading mb={4} color="white">Your Profile</Heading>
+        <Flex direction="column" align="center" justify="center" minHeight="calc(100vh - 64px)">
+          <Heading mb={4} color="white">Your Profile</Heading>
 
-                <Box w="full" maxW="600px" p={4} bgColor={boxColor} borderWidth={1} borderRadius="lg" boxShadow="lg">
-                    {user ? (
-                        <HStack spacing={4}>
-                            <Text color="white">Username: {user.username || 'Not set'}</Text>
-                            <Text color="white">Email: {user.email}</Text>
-                            {/* More profile details */}
-                        </HStack>
-                    ) : (
-                        <Text color="white">Loading or not logged in...</Text>
-                    )}
-                </Box>
-            </Flex>
-        </Box>
+          <Box w="full" maxW="600px" p={4} bgColor={boxColor} borderWidth={1} borderRadius="lg" boxShadow="lg">
+            {user ? (
+              <HStack spacing={4}>
+                <Text color="white">Email: {user.email}</Text>
+                {/* More profile details */}
+                <Button colorScheme="red" onClick={handleDeleteAccount}>Delete Account</Button>
+              </HStack>
+            ) : (
+              <Text color="white">Loading or not logged in...</Text>
+            )}
+          </Box>
+        </Flex>
+      </Box>
     </ChakraProvider>
   );
 }
