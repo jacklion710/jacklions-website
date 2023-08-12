2ea05b7f941d88a580eb7f7610f7d8521f0967fb
jacklion710 <jack.lion710@gmail.com>
2023-08-11 20:31:04 -0400

    "Added user bio"

--- pages/Profile.tsx
+++ pages/Profile.tsx
@@ -1,10 +1,11 @@
-import { Box, ChakraProvider, Flex, Heading, Text, HStack, Button, useToast } from '@chakra-ui/react';
+import { Box, ChakraProvider, Flex, Heading, Text, VStack, HStack, Input, Button, useToast } from '@chakra-ui/react';
 import Navbar from '../components/Navbar';
 import { onAuthStateChanged, deleteUser } from 'firebase/auth';
 import { useState, useEffect } from 'react';
 import { getFirestore, doc, setDoc, getDoc, deleteDoc } from 'firebase/firestore';
 import { auth } from '@/utils/firebase';
 import { useRouter } from 'next/router';
+import React from 'react';
 
 interface User {
   email: string | null;
@@ -18,6 +19,22 @@ const Profile = () => {
   const boxColor = 'gray.700';  
   const toast = useToast();
   const router = useRouter();
+  const [bio, setBio] = useState<string | null>(null);
+  const [editing, setEditing] = useState<boolean>(false);
+  const [tempBio, setTempBio] = useState<string>('');
+
+  const handleSaveBio = async () => {
+    if (user && auth.currentUser) {
+        const db = getFirestore();
+        const userDocRef = doc(db, 'users', auth.currentUser.uid);
+        
+        await setDoc(userDocRef, { bio: tempBio }, { merge: true });
+
+        setBio(tempBio);
+        setEditing(false);
+    }
+};
+
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
@@ -27,10 +44,13 @@ const Profile = () => {
         if (userDoc.exists()) {
           const userData = userDoc.data();
           setUser({
-            email: currentUser.email,
-            displayName: currentUser.displayName,
-            username: userData.username || null,
+              email: currentUser.email,
+              displayName: currentUser.displayName,
+              username: userData.username || null,
           });
+          setBio(userData.bio || null);
+
+          setBio(userData.bio || null); // Set the bio here
         } else {
           setUser({
             email: currentUser.email,
@@ -80,32 +100,51 @@ const Profile = () => {
       }
     }
   };
+
+  
   
 
   return (
     <ChakraProvider>
-      <Box bgColor={backgroundColor} minH="100vh">
-        <Navbar />
+        <Box bgColor={backgroundColor} minH="100vh">
+            <Navbar />
 
-        <Flex direction="column" align="center" justify="center" minHeight="calc(100vh - 64px)">
-          <Heading mb={4} color="white">Your Profile</Heading>
+            <Flex direction="column" align="center" justify="center" minHeight="calc(100vh - 64px)">
+                <Heading mb={4} color="white">Your Profile</Heading>
 
-          <Box w="full" maxW="600px" p={4} bgColor={boxColor} borderWidth={1} borderRadius="lg" boxShadow="lg">
-            {user ? (
-              <HStack spacing={4}>
-                <Text color="white">Username: {user.username}</Text>
-                <Text color="white">Email: {user.email}</Text>
-                {/* More profile details */}
-                <Button colorScheme="red" onClick={handleDeleteAccount}>Delete Account</Button>
-              </HStack>
-            ) : (
-              <Text color="white">Loading or not logged in...</Text>
-            )}
-          </Box>
-        </Flex>
-      </Box>
+                <Box w="full" maxW="600px" p={4} bgColor={boxColor} borderWidth={1} borderRadius="lg" boxShadow="lg">
+                    {user ? (
+                        <VStack spacing={4} align="start">
+                            <Text color="white">Username: {user.username}</Text>
+                            <Text color="white">Email: {user.email}</Text>
+                            
+                            {/* Bio section */}
+                            {!editing ? (
+                                <Box>
+                                    <Text color="white">Bio: {bio || "No bio available."}</Text>
+                                    <Button mt={2} size="sm" onClick={() => { setEditing(true); setTempBio(bio || ''); }}>Edit Bio</Button>
+                                </Box>
+                            ) : (
+                                <Box>
+                                    <Input 
+                                        value={tempBio} 
+                                        onChange={(e) => setTempBio(e.target.value)} 
+                                    />
+                                    <Button mt={2} size="sm" onClick={handleSaveBio}>Save</Button>
+                                </Box>
+                            )}
+                            
+                            {/* Delete Account Button */}
+                            <Button colorScheme="red" onClick={handleDeleteAccount}>Delete Account</Button>
+                        </VStack>
+                    ) : (
+                        <Text color="white">Loading or not logged in...</Text>
+                    )}
+                </Box>
+            </Flex>
+        </Box>
     </ChakraProvider>
-  );
+);
 }
 
 export default Profile;
