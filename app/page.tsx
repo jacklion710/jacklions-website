0005210b77ae16e4b06b58e6f87f32503b52b1df
jacklion710 <jack.lion710@gmail.com>
2023-08-12 13:37:31 -0400

    "Moved footer to layout"

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 "use client"
 import Features from '@/components/Features'
-import Footer from '@/components/Footer'
 import './globals.css';
 
 export default function Home() {
   return (
     <>
     <Features />
-    <Footer />
     </>
   )
 }
