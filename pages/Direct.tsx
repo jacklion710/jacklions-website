ba55f469b92e2e86856fc495cc0d904c670f41b5
jacklion710 <jack.lion710@gmail.com>
2023-08-21 22:05:22 -0400

    Added flexbox layer for entire page

--- pages/Direct.tsx
+++ pages/Direct.tsx
@@ -24,67 +24,69 @@ export default function SocialPage() {
 
   return (
     <ChakraProvider>
-      <Navbar />
-      <Flex
-        direction="column"
-        align="center"
-        justify="flex-start"
-        py={8}
-        px={4}
-        height="calc(98vh - 100px)"
-      >
-        <Box 
-          w="100%" 
-          maxW="600px" 
-          textAlign="center" 
-          position="relative"
-        >
-          <Heading 
-            size="xl" 
-            mb={4} 
-            position="relative"
-            _after={{
-              content: '""',
-              display: 'block',
-              position: 'absolute',
-              bottom: '-10px',  // Adjust this value to move the border closer/farther from the text
-              left: '50%',
-              transform: 'translateX(-50%)',
-              width: '290px',  // Adjust this width as per your requirements
-              borderBottom: `1px solid `,
-            }}
+        <Flex direction="column" minHeight="100vh">
+          <Navbar />
+          <Flex
+            direction="column"
+            align="center"
+            justify="flex-start"
+            py={8}
+            px={4}
+            flexGrow={1}
           >
-            Connect with Me
-          </Heading>
-          <Text mb={6}>
-            Want to ask a question or book me for a gig? Reach out to me directly on any{' '}
-            <Link href="/Direct" color="teal.500">platform</Link> or send me an <Link href="mailto:jack.lion710@gmail.com" color="teal.500">email</Link>
-        </Text>
-          <HStack spacing={4} justify="center" wrap="wrap" mb={4}>
-            <Link href="https://www.instagram.com/jack.lion/?hl=en" isExternal>
-              <IconButton aria-label="Spotify" icon={<FaInstagram />} size="lg" colorScheme="pink" />
-            </Link>
-            {/* ... other icons ... */}
-            <Link href="mailto:jack.lion710@gmail.com">
-              <IconButton aria-label="Email" icon={<FaEnvelope />} size="lg" colorScheme="teal" />
-            </Link>
-          </HStack>
-
-          {/* Bookings Section */}
-          <VStack spacing={4} align="center" mt={10}>
-            <Text fontSize="lg" fontWeight="semibold">
-              For bookings, please use the link below:
+          <Box 
+            w="100%" 
+            maxW="600px" 
+            textAlign="center" 
+            position="relative"
+          >
+            <Heading 
+              size="xl" 
+              mb={4} 
+              position="relative"
+              _after={{
+                content: '""',
+                display: 'block',
+                position: 'absolute',
+                bottom: '-10px',  // Adjust this value to move the border closer/farther from the text
+                left: '50%',
+                transform: 'translateX(-50%)',
+                width: '290px',  // Adjust this width as per your requirements
+                borderBottom: `1px solid `,
+              }}
+            >
+              Connect with Me
+            </Heading>
+            <Text mb={6}>
+              Want to ask a question or book me for a gig? Reach out to me directly on any{' '}
+              <Link href="/Direct" color="teal.500">platform</Link> or send me an <Link href="mailto:jack.lion710@gmail.com" color="teal.500">email</Link>
             </Text>
-              <Button colorScheme="blue" size="md" mt={4}>
-                  <Link href="/SessionInfo">
-                      Book a Session
-                  </Link>
-              </Button>
+              <HStack spacing={4} justify="center" wrap="wrap" mb={4}>
+                <Link href="https://www.instagram.com/jack.lion/?hl=en" isExternal>
+                  <IconButton aria-label="Spotify" icon={<FaInstagram />} size="lg" colorScheme="pink" />
+                </Link>
+                {/* ... other icons ... */}
+                <Link href="mailto:jack.lion710@gmail.com">
+                  <IconButton aria-label="Email" icon={<FaEnvelope />} size="lg" colorScheme="teal" />
+                </Link>
+              </HStack>
 
-          </VStack>
-        </Box>
+              {/* Bookings Section */}
+              <VStack spacing={4} align="center" mt={10}>
+                <Text fontSize="lg" fontWeight="semibold">
+                  For bookings, please use the link below:
+                </Text>
+                  <Button colorScheme="blue" size="md" mt={4}>
+                      <Link href="/SessionInfo">
+                          Book a Session
+                      </Link>
+                  </Button>
+
+                </VStack>
+              </Box>
+            </Flex>
+          <Footer />
       </Flex>
-      <Footer />  
     </ChakraProvider>
   );
 }
