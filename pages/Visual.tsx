918d462442bd57137ca517b2a389a36bed68e152
jacklion710 <jack.lion710@gmail.com>
2023-08-21 23:29:18 -0400

    Optimized code for maintenance

--- pages/Visual.tsx
+++ pages/Visual.tsx
@@ -9,79 +9,44 @@ import {
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
-export default function HomePage() {
+// Define the list of Instagram embed URLs
+const embedURLs = [
+  "https://www.instagram.com/p/Cpx9BBMp1tG/embed",
+  "https://www.instagram.com/p/CEXJReCgq5h/embed",
+  "https://www.instagram.com/p/CFtCJm_As_E/embed",
+  "https://www.instagram.com/p/ChxYt1Cp1WT/embed",
+  "https://www.instagram.com/p/Co5o9b4M9lV/embed",
+  "https://www.instagram.com/p/CptSIkPAF_I/embed"
+];
 
+export default function HomePage() {
   return (
     <ChakraProvider>
       <Navbar />
       <Flex justify="center" my={8} ml={0} flexGrow={1}>
         <Flex direction={["column", "row"]} width="100%" pb="100px">
-            <Box p={2} flexGrow={1}>
-              <VStack spacing={8} align="center">
-                <Heading size="xl" borderBottom="1px solid">Visuals</Heading>
-        <SimpleGrid columns={{ base: 1, md: 3 }} spacing={20}>
-          <Box>
-              <iframe 
-                  src="https://www.instagram.com/p/Cpx9BBMp1tG/embed" 
-                  width="100%" 
-                  height="480" 
-                  frameBorder="0" 
-                  scrolling="no" 
-              ></iframe>
+          <Box p={2} flexGrow={1}>
+            <VStack spacing={8} align="center">
+              <Heading size="xl" borderBottom="1px solid">Visuals</Heading>
+              <SimpleGrid columns={{ base: 1, md: 3 }} spacing={20}>
+                {embedURLs.map(url => (
+                  <Box key={url}>
+                    <iframe 
+                      src={url} 
+                      width="100%" 
+                      height="480" 
+                      frameBorder="0" 
+                      scrolling="no" 
+                    ></iframe>
+                  </Box>
+                ))}
+              </SimpleGrid>
+            </VStack>
           </Box>
-          <Box>
-            <iframe 
-                src="https://www.instagram.com/p/CEXJReCgq5h/embed" 
-                width="100%" 
-                height="480" 
-                frameBorder="0" 
-                scrolling="no" 
-            ></iframe>
-          </Box>
-          <Box>
-            <iframe 
-                src="https://www.instagram.com/p/CFtCJm_As_E/embed" 
-                width="100%" 
-                height="480" 
-                frameBorder="0" 
-                scrolling="no" 
-            ></iframe>
-          </Box>
-          <Box>
-            <iframe 
-                src="https://www.instagram.com/p/ChxYt1Cp1WT/embed" 
-                width="100%" 
-                height="480" 
-                frameBorder="0" 
-                scrolling="no" 
-            ></iframe>
-          </Box>
-          <Box>
-            <iframe 
-                src="https://www.instagram.com/p/Co5o9b4M9lV/embed" 
-                width="100%" 
-                height="480" 
-                frameBorder="0" 
-                scrolling="no" 
-            ></iframe>
-          </Box>
-          <Box>
-            <iframe 
-                src="https://www.instagram.com/p/CptSIkPAF_I/embed" 
-                width="100%" 
-                height="480" 
-                frameBorder="0" 
-                scrolling="no" 
-            ></iframe>
-          </Box>
-                </SimpleGrid>
-              </VStack>
-            </Box>
-          {/* Right empty panel */}
           <Box w={{ base: "0%", md: "1%" }} />
         </Flex>
       </Flex>
-    <Footer/>
-  </ChakraProvider>
-);
+      <Footer/>
+    </ChakraProvider>
+  );
 }
