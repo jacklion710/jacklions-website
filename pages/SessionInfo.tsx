d42b54fee2842b918a3f7394b7ce1447d6a3d78e
jacklion710 <jack.lion710@gmail.com>
2023-08-21 23:38:13 -0400

    "Optimized instructions for maintenance"

--- pages/SessionInfo.tsx
+++ pages/SessionInfo.tsx
@@ -9,14 +9,24 @@ import {
     List,
     ListItem,
     Link
-  } from '@chakra-ui/react';
-  import Navbar from '../components/Navbar';
-  import Footer from '../components/Footer';
+} from '@chakra-ui/react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
-  export default function BookingInstructionsPage() {
+// Define the booking instructions
+const bookingInstructions = [
+    "Click on the 'Book Now' button below.",
+    "Select an available date and time from the calendar.",
+    "Fill in your personal information and any additional notes as well as what kind of session you are booking (recording session or lesson).",
+    "Confirm the appointment and you'll receive a confirmation email shortly."
+];
+
+export default function BookingInstructionsPage() {
     return (
         <ChakraProvider>
             <Navbar />
+
+            {/* Main content */}
             <Flex justify="center" my={8} ml={0} flexGrow={1} pb="460px">
                 <VStack spacing={10} width="100%" maxW="800px">
                     <Heading size="xl" borderBottom="1px solid">Booking Instructions</Heading>
@@ -24,33 +34,28 @@ import {
                     <Box p={4} shadow="lg" borderWidth="1px" borderRadius="md" width="100%">
                         <Text mb={4}>Follow these easy steps to book your appointment:</Text>
                         
+                        {/* Booking steps */}
                         <List spacing={2}>
-                            <ListItem>
-                                <Text fontSize="md">1. Click on the &apos;Book Now&apos; button below.</Text>
-                            </ListItem>
-                            <ListItem>
-                                <Text fontSize="md">2. Select an available date and time from the calendar.</Text>
-                            </ListItem>
-                            <ListItem>
-                                <Text fontSize="md">3. Fill in your personal information and any additional notes as well as what kind of session you are booking (recording session or lesson).</Text>
-                            </ListItem>
-                            <ListItem>
-                                <Text fontSize="md">4. Confirm the appointment and you&apos;ll receive a confirmation email shortly.</Text>
-                            </ListItem>
+                            {bookingInstructions.map((instruction, index) => (
+                                <ListItem key={index}>
+                                    <Text fontSize="md">{`${index + 1}. ${instruction}`}</Text>
+                                </ListItem>
+                            ))}
                         </List>
   
+                        {/* Book Now button */}
                         <Flex justify="center">
                             <Button colorScheme="blue" size="md" mt={6}>
                                 <Link href="https://calendar.google.com/calendar/u/0/appointments/schedules/AcZssZ0w9Y-bSp4iEfmtZLVteHAJSyjUOXOf_pLSQx0JAN_XalobFO2LVchz5a_qWVwUqusj5KcKVPth" isExternal>
-                                Book Now
+                                    Book Now
                                 </Link>
                             </Button>
                         </Flex>
                     </Box>
                 </VStack>
             </Flex>
-        <Footer />
-    </ChakraProvider>
-);
+
+            <Footer />
+        </ChakraProvider>
+    );
 }
-  
