2fde19d26504d6529a1e415b19b88a5f2e69262f
jacklion710 <jack.lion710@gmail.com>
2023-09-16 16:18:14 -0400

    Editted mastering instructions

--- pages/Engineering.tsx
+++ pages/Engineering.tsx
@@ -124,7 +124,7 @@ export default function EngineeringPage() {
                         <Heading size="sm" mt={2}>For Mastering:</Heading>
                         <Text>- Provide the final mixed down track in the highest resolution possible.</Text>
                         <Text>- There should be no mastering such as limiting applied to the mix.</Text>
-                        <Text>- Ensure there's a minimum of -6dB headroom.</Text>
+                        <Text>- Ensure there's a at least -12 to -6dB headroom.</Text>
                         <Text>- Provide any reference tracks or specific instructions.</Text>
                     </Box>
 
