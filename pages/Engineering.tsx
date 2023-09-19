14db18a40b3940ec87ba55123b5e7e775b78353e
jacklion710 <jack.lion710@gmail.com>
2023-09-19 15:47:45 -0400

    Added audio for mastering comparisons

--- pages/Engineering.tsx
+++ pages/Engineering.tsx
@@ -9,6 +9,8 @@ import {
   Button,
   List
 } from '@chakra-ui/react';
+import React, { useEffect, useRef } from 'react';
+import WaveSurfer from 'wavesurfer.js';
 import { 
     FaMicrophone, 
     FaHeadphones,
@@ -22,6 +24,61 @@ import Footer from '../components/Footer';
 import Head from 'next/head';
 
 export default function EngineeringPage() {
+    const waveformRefPre = useRef(null);
+    const waveformRefAnalog = useRef(null);
+    const waveformRefDigital = useRef(null);
+    const wavesurferPre = useRef<WaveSurfer | null>(null);
+    const wavesurferAnalog = useRef<WaveSurfer | null>(null);
+    const wavesurferDigital = useRef<WaveSurfer | null>(null);
+    const wavesurfer = useRef<WaveSurfer | null>(null);
+  
+    useEffect(() => {
+        if (waveformRefPre.current) {
+           wavesurferPre.current = WaveSurfer.create({
+              container: waveformRefPre.current,
+              waveColor: '#2C3E50',
+              progressColor: '#3498DB'
+           });
+           wavesurferPre.current.load('/audio/Long_Road[Premaster].wav');
+        }
+     
+        if (waveformRefAnalog.current) {
+           wavesurferAnalog.current = WaveSurfer.create({
+              container: waveformRefAnalog.current,
+              waveColor: '#2C3E50',
+              progressColor: '#3498DB'
+           });
+           wavesurferAnalog.current.load('/audio/Long_Road[Analog_Master].wav');
+        }
+
+        if (waveformRefDigital.current) {
+            wavesurferDigital.current = WaveSurfer.create({
+               container: waveformRefDigital.current,
+               waveColor: '#2C3E50',
+               progressColor: '#3498DB'
+            });
+            wavesurferDigital.current.load('/audio/Long_Road[Digital_Master].wav');
+         }
+     
+        return () => {
+           wavesurferPre.current?.destroy();
+           wavesurferAnalog.current?.destroy();
+           wavesurferDigital.current?.destroy();
+        };
+     }, []);
+
+     const handlePlayPausePre = () => {
+        wavesurferPre.current?.playPause();
+     };
+     
+     const handlePlayPauseAnalog = () => {
+        wavesurferAnalog.current?.playPause();
+     };
+
+     const handlePlayPauseDigital = () => {
+        wavesurferDigital.current?.playPause();
+     };
+
   return (
       <ChakraProvider>
         <Head>
@@ -37,6 +94,68 @@ export default function EngineeringPage() {
           <Flex justify="center" my={8} ml={0} flexGrow={1} pb="100px">
             <VStack spacing={10} width="100%" maxW="600px">
                   <Heading size="xl" borderBottom="1px solid"> Recording Studio Catalog</Heading>
+
+                  <Box p={4} shadow="lg" borderWidth="1px" borderRadius="md" width="100%">
+                        <Flex align="center">
+                            <Heading size="lg" mb={4}>Mixing and Mastering</Heading>
+                            <Box ml={4} mt={-2}>
+                                <FaWrench size="1.5em" />
+                            </Box>
+                        </Flex>
+                        <Heading size="md" mt={4}>Digital Mastering:</Heading>
+                        <Text>$25 for single track</Text>
+                        <Text>$100 for an EP (3 - 5 tracks)</Text>
+                        <Text>$200 for an album (6 - 12 tracks)</Text>
+                        
+                        <Heading size="md" mt={4}>Analog Mastering:</Heading>
+                        <Text>$40 for single track</Text>
+                        <Text>$180 for an EP (3 - 5 tracks)</Text>
+                        <Text>$360 for an album (6 - 12 tracks)</Text>
+                        
+                        <Flex flexDirection="column" alignItems="center" mt={4}>
+                            <Heading size="sm">Premaster</Heading>
+                            <Box mt={4} mb={10} width="100%" height="100px"> 
+                                <div ref={waveformRefPre} style={{ width: '100%', height: '100%' }}></div>
+                            </Box>
+                            <Button onClick={handlePlayPausePre}>Play/Pause</Button>
+                        </Flex>
+
+                        <Flex flexDirection="column" alignItems="center" mt={4}>
+                            <Heading size="sm">Analog Master</Heading>
+                            <Box mt={4} mb={10} width="100%" height="100px"> 
+                                <div ref={waveformRefAnalog} style={{ width: '100%', height: '100%' }}></div>
+                            </Box>
+                            <Button onClick={handlePlayPauseAnalog}>Play/Pause</Button>
+                        </Flex>
+
+                        <Flex flexDirection="column" alignItems="center" mt={4}>
+                            <Heading size="sm">Digital Master</Heading>
+                            <Box mt={4} mb={10} width="100%" height="100px"> 
+                                <div ref={waveformRefDigital} style={{ width: '100%', height: '100%' }}></div>
+                            </Box>
+                            <Button onClick={handlePlayPauseDigital}>Play/Pause</Button>
+                        </Flex>
+
+                        <Heading size="md" mt={4}>Important Notes:</Heading>
+                        <Heading size="sm" mt={2}>For Mixdowns:</Heading>
+                        <Text>- Ensure each track is exported individually without effects such as compression and EQ unless it is crucial to the sound.</Text>
+                        <Text>- Avoid clipping. Ensure the peak level of each track is below 0dB.</Text>
+                        <Text>- Provide any reference tracks or specific instructions.</Text>
+
+                        <Heading size="sm" mt={2}>For Mastering:</Heading>
+                        <Text>- Provide the final mixed down track in the highest resolution possible.</Text>
+                        <Text>- There should be no mastering such as limiting applied to the mix.</Text>
+                        <Text>- Ensure that there is a at least -12 to -6dB headroom.</Text>
+                        <Text>- Provide any reference tracks or specific instructions.</Text>
+                        <Flex justifyContent="center" alignItems="center" mt={4}>
+                            <Button colorScheme="blue" size="md">
+                                <Link href="/Direct">
+                                    For Engineering Services, Contact Me
+                                </Link>
+                            </Button>
+                        </Flex>
+                    </Box>
+                    
 
                   <Box p={4} shadow="lg" borderWidth="1px" borderRadius="md" width="100%">
                     <Flex direction="column" justifyContent="space-between" alignItems="flex-start" h="100%">
@@ -82,18 +201,6 @@ export default function EngineeringPage() {
                     </Flex>
                 </Box>
 
-                <Box p={4} shadow="lg" borderWidth="1px" borderRadius="md" width="100%">
-                    <Flex align="center">
-                        <Heading size="lg" mb={4}>Engineering</Heading>
-                        <Box ml={4} mt={-2}>
-                            <FaHeadphones size="1.5em" />
-                        </Box>
-                    </Flex>
-                      <Text>Choose from a variety of mixdown and master options to get the perfect sound for your project.</Text>
-                      <Text>Mixdown</Text>
-                      <Text>Digital Mastering</Text>
-                      <Text>Analog Mastering</Text>
-                  </Box>
                   <Box p={4} shadow="lg" borderWidth="1px" borderRadius="md" width="100%">
                     <Flex align="center">
                         <Heading size="lg" mb={4}>Sound Design</Heading>
@@ -124,43 +231,6 @@ export default function EngineeringPage() {
                       <Text>Art Installation Planning</Text>
                       <Text>DSP Theory & Design</Text>
                   </Box>
-
-                  <Button colorScheme="blue" size="md" mt={4}>
-                      <Link href="/Direct">
-                          For Engineering Services, Contact Me
-                      </Link>
-                  </Button>
-
-                    <Box p={4} shadow="lg" borderWidth="1px" borderRadius="md" width="100%">
-                        <Flex align="center">
-                            <Heading size="lg" mb={4}>Mixing and Mastering</Heading>
-                            <Box ml={4} mt={-2}>
-                                <FaWrench size="1.5em" />
-                            </Box>
-                        </Flex>
-                        <Heading size="md" mt={4}>Digital Mastering:</Heading>
-                        <Text>$25 for single track</Text>
-                        <Text>$100 for an EP (3 - 5 tracks)</Text>
-                        <Text>$200 for an album (6 - 12 tracks)</Text>
-                        
-                        <Heading size="md" mt={4}>Analog Mastering:</Heading>
-                        <Text>$40 for single track</Text>
-                        <Text>$180 for an EP (3 - 5 tracks)</Text>
-                        <Text>$360 for an album (6 - 12 tracks)</Text>
-
-                        <Heading size="md" mt={4}>Important Notes:</Heading>
-                        <Heading size="sm" mt={2}>For Mixdowns:</Heading>
-                        <Text>- Ensure each track is exported individually without effects such as compression and EQ unless it is crucial to the sound.</Text>
-                        <Text>- Avoid clipping. Ensure the peak level of each track is below 0dB.</Text>
-                        <Text>- Provide any reference tracks or specific instructions.</Text>
-
-                        <Heading size="sm" mt={2}>For Mastering:</Heading>
-                        <Text>- Provide the final mixed down track in the highest resolution possible.</Text>
-                        <Text>- There should be no mastering such as limiting applied to the mix.</Text>
-                        <Text>- Ensure that there is a at least -12 to -6dB headroom.</Text>
-                        <Text>- Provide any reference tracks or specific instructions.</Text>
-                    </Box>
-
               </VStack>
           </Flex>
         <Footer />  
